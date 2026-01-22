c493e6bfaa5e2230c376d0903d199c2ed9141d5b
Anna Jungmannova <ajungmannova@gmail.com>
2026-01-22 15:30:39 +0000

    Project model and readme

--- train.py
+++ train.py
@@ -55,13 +55,16 @@ def main():
     # Data is located at:
     # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-    ds = ### YOUR CODE HERE ###
+    #ds = ### YOUR CODE HERE ###
+    csv_path = "https://raw.githubusercontent.com/timothywarner/ai100/refs/heads/master/bankmarketing_train.csv"
+    ds = TabularDatasetFactory.from_delimited_files(csv_path)
     
     x, y = clean_data(ds)
 
     # TODO: Split data into train and test sets.
 
-    ### YOUR CODE HERE ###a
+    ### YOUR CODE HERE ###
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 2026)
 
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
